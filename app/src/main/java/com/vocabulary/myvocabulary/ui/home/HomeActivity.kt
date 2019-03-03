6b1fca870e5ce290f16b47076c3e3634dd5f03e0
VargaJohanna <varga.johanna@gmail.com>
2019-03-03 10:44:28 +0000

    Clean up and start over

--- app/src/main/java/com/vocabulary/myvocabulary/ui/home/HomeActivity.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/home/HomeActivity.kt
@@ -1,13 +1,9 @@
 package com.vocabulary.myvocabulary.ui.home
 
 import android.annotation.SuppressLint
-import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.vocabulary.myvocabulary.R
-import kotlinx.android.synthetic.main.activity_home.*
-import android.support.design.widget.TabLayout
-
 
 
 class HomeActivity : AppCompatActivity() {
@@ -17,27 +13,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        tabView.addTab(tabView.newTab().setText("Dictionaries"))
-        tabView.addTab(tabView.newTab().setText("Tests"))
-//        tabView.tabGravity(TabLayout.GRAVITY_FILL)
 
-        val sectionAdapter = PagerAdapter(supportFragmentManager)
-        homeViewPager.adapter = sectionAdapter
-        tabView.setupWithViewPager(homeViewPager)
-        tabView.setTabTextColors(R.color.primary_text_default_material_dark, Color.WHITE)
-
-        tabView.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                homeViewPager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-
-            }
-        })
     }
 }
