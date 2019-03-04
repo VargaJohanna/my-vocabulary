b49eb97e5ecce2f5ee02441de1061155a8433328
VargaJohanna <varga.johanna@gmail.com>
2019-03-04 21:18:51 +0000

    Add navigation component and dictionary, quizzes list empty fragments

---
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/quizzes/QuizListFragment.kt
@@ -0,0 +1,14 @@
+package com.vocabulary.myvocabulary.ui.quizzes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vocabulary.myvocabulary.R
+
+class QuizListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_quiz_list, container, false)
+    }
+}
