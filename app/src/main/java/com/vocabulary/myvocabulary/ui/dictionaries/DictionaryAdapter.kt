8668758310e8c7d4eb9eca6c8fd8cdce344ecf79
VargaJohanna <varga.johanna@gmail.com>
2019-03-05 11:02:02 +0000

    Create dictionary adapter

---
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/dictionaries/DictionaryAdapter.kt
@@ -0,0 +1,40 @@
+package com.vocabulary.myvocabulary.ui.dictionaries
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vocabulary.myvocabulary.R
+
+class DictionaryAdapter(private val dictionaryList : List<Dictionary>, private val itemClickListener: ItemClickListener) : RecyclerView.Adapter<DictionaryAdapter.DictionaryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DictionaryViewHolder {
+        val inflater =LayoutInflater.from(parent.context)
+        return DictionaryViewHolder(inflater.inflate(R.layout.row_dictionary, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return dictionaryList.size
+    }
+
+    override fun onBindViewHolder(holder: DictionaryViewHolder, position: Int) {
+        holder.dictionaryName.setText(dictionaryList.get(position).getName())
+    }
+
+    inner class DictionaryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        val dictionaryName : TextView = itemView.findViewById(R.id.dictionary_name)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            itemClickListener.onItemClick(dictionaryList[adapterPosition])
+        }
+    }
+
+    interface ItemClickListener {
+        fun onItemClick(dictionary: Dictionary)
+    }
+}
