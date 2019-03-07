2ac0bfaeb0b3dd0d29b6d17a51a682d574408fc1
VargaJohanna <varga.johanna@gmail.com>
2019-03-07 15:56:53 +0000

    More clean up

--- app/src/main/java/com/vocabulary/myvocabulary/room/wordData/WordRepository.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/room/wordData/WordRepository.kt
@@ -1,12 +1,12 @@
 package com.vocabulary.myvocabulary.room.wordData
 
 import com.vocabulary.myvocabulary.ui.words.Word
-import io.reactivex.Observable
+import io.reactivex.Single
 
 interface WordRepository {
     fun createWord(word: Word)
     fun deleteWord(word: Word)
     fun updateWord(word: Word)
-    fun getAllWords(): Observable<List<Word>>
-    fun getWordById(wordId: Long): Observable<Word>
+    fun getAllWords(): Single<List<Word>>
+    fun getWordById(wordId: Long): Single<Word>
 }
