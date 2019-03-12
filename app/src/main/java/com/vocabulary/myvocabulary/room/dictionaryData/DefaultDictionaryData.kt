20f747bc01e827eadd1eb6c8671fe11ec93f18ed
VargaJohanna <varga.johanna@gmail.com>
2019-03-12 11:31:12 +0000

    Add dictionary title and column titles to word list layout

--- app/src/main/java/com/vocabulary/myvocabulary/room/dictionaryData/DefaultDictionaryData.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/room/dictionaryData/DefaultDictionaryData.kt
@@ -3,7 +3,7 @@ package com.vocabulary.myvocabulary.room.dictionaryData
 import com.vocabulary.myvocabulary.ui.dictionaries.Dictionary
 import java.util.*
 
-class DefaultDictionary {
+class DefaultDictionaryData {
     fun getDefaultDictionary() =
             Dictionary(dictionaryName = "Hungarian Animals", dictionaryCreated =  Calendar.getInstance().time)
 }
