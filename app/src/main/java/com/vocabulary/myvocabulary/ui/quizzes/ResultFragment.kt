dae06916476e6d3de20e45f1b34df04ba3252a31
VargaJohanna <varga.johanna@gmail.com>
2019-03-18 10:29:41 +0000

    Add result to navigation

---
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/quizzes/ResultFragment.kt
@@ -0,0 +1,15 @@
+package com.vocabulary.myvocabulary.ui.quizzes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vocabulary.myvocabulary.R
+
+class ResultFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+}
