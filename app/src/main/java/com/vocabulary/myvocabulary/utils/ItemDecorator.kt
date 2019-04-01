abfb3d1481436080c3260e68463a27b17e889f10
VargaJohanna <varga.johanna@gmail.com>
2019-04-01 11:20:17 +0100

    Set offset based on previous item's height

--- app/src/main/java/com/vocabulary/myvocabulary/utils/ItemDecorator.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/utils/ItemDecorator.kt
@@ -1,5 +1,6 @@
 package com.vocabulary.myvocabulary.utils
 
+import android.graphics.Canvas
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +8,19 @@ import androidx.recyclerview.widget.RecyclerView
 class ItemDecorator(private val intSpace: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.top = intSpace
+        var previousHeight: Int
+        val currentItemPosition = parent.indexOfChild(view)
+        val previousItem = parent.getChildAt(currentItemPosition - 1)
+
+        if(currentItemPosition != 0) {
+            previousHeight = previousItem.height
+            view.minimumHeight = previousHeight
+
+        } else {
+            previousHeight = 120 + intSpace
+        }
+        val actual = -(previousHeight - intSpace)
+        outRect.top = actual
     }
+
 }
