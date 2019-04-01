cb027f2127d6bd4b0102f6d9a185b744fb097c2a
VargaJohanna <varga.johanna@gmail.com>
2019-04-01 22:06:34 +0100

    Turn asked question grey

--- app/src/main/java/com/vocabulary/myvocabulary/ui/quizzes/QuizFragment.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/quizzes/QuizFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -98,7 +97,7 @@ class QuizFragment : Fragment() {
                 override fun canScrollVertically(): Boolean = false
             }
 
-            addItemDecoration(ItemDecorator(50))
+            addItemDecoration(ItemDecorator(80))
             adapter = quizAdapter
         }
     }
