15b9939d2d058f3cef11c82ff82c934e6f6c6edc
VargaJohanna <varga.johanna@gmail.com>
2019-08-17 21:14:52 +0100

    Search works

--- app/src/main/java/com/vocabulary/myvocabulary/ext/ViewExtensions.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ext/ViewExtensions.kt
@@ -17,3 +17,7 @@ fun View.display(displayed: Boolean) {
         View.GONE
     }
 }
+
+fun View.displayWithAnimation(displayed: Boolean) {
+    this.animate().translationY(this.height.toFloat())
+}
