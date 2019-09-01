b3286c19b34d190c751018b811e7bf656ae0c747
VargaJohanna <varga.johanna@gmail.com>
2019-09-01 09:42:00 +0100

    Add click listener and show number of words

--- app/src/main/java/com/vocabulary/myvocabulary/ui/words/NumberOfWordsItem.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/words/NumberOfWordsItem.kt
@@ -3,13 +3,22 @@ package com.vocabulary.myvocabulary.ui.words
 import com.vocabulary.myvocabulary.R
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.row_number_of_words.view.*
 
 
-class NumberOfWordsItem(numberOfWords: String) : Item(-1) {
+class NumberOfWordsItem(
+        private val numberOfWords: String
+) : Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-
+        viewHolder.containerView.apply {
+            number_of_words.text = numberOfWords
+        }
     }
 
-    override fun getLayout() = R.layout.row_word
+    override fun getLayout() = R.layout.row_number_of_words
+
+//    override fun isSameAs(other: com.xwray.groupie.Item<*>?) =  numberOfWords == (other as? NumberOfWordsItem)?.numberOfWords
+//
+//    override fun equals(other: Any?) = numberOfWords == (other as? NumberOfWordsItem)?.numberOfWords
 
 }
--- app/src/main/java/com/vocabulary/myvocabulary/ui/words/WordItem.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/words/WordItem.kt
@@ -6,19 +6,21 @@ import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.row_word.view.*
 
 class WordItem(
-        private val wordData: Word
+        val wordData: Word,
+        private val onClick: (selectedItem: WordItem) -> Unit
 ) : Item(wordData.wordId) {
-    override fun bind(viewHolder: ViewHolder, position: Int) {
 
+    override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.containerView.apply {
             word.text = wordData.word
             translation.text = wordData.translation
+            setOnClickListener { onClick(this@WordItem) }
         }
     }
 
     override fun getLayout() = R.layout.row_word
 
-    override fun isSameAs(other: com.xwray.groupie.Item<*>?) =  wordData == (other as? WordItem)?.wordData
+    override fun isSameAs(other: com.xwray.groupie.Item<*>?) = wordData.wordId == (other as? WordItem)?.wordData?.wordId
 
-    override fun equals(other: Any?) = wordData == (other as? WordItem)?.wordData
+    override fun equals(other: Any?) = wordData.wordId == (other as? WordItem)?.wordData?.wordId
 }
