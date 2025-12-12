e216a7e9f40f94f0fb8eef2dc5defc6153033b6f
johannavarga <varga.johanna@gmail.com>
2025-12-12 09:31:50 +0100

    Add edit, delete dictionary dialog

---
+++ app/src/main/java/com/vocabulary/myvocabulary/utils/ComposeDialogFactory.kt
@@ -0,0 +1,187 @@
+package com.vocabulary.myvocabulary.utils
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vocabulary.myvocabulary.R
+
+class ComposeDialogFactory {
+    @Composable
+    fun BuildCreateDictionaryDialog(
+        onDismissRequest: () -> Unit,
+        onConfirmation: (newTitle: String) -> Unit,
+        dialogTitle: String,
+    ) {
+        val newDictionaryTitleState = rememberTextFieldState()
+        AlertDialog(
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                OutlinedTextField(
+                    state = newDictionaryTitleState,
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(8.dp),
+                    label = { Text(stringResource(R.string.create_dictionary_hint)) },
+                    supportingText = {}
+                )
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation(newDictionaryTitleState.text.toString())
+                    }
+                ) {
+                    Text(stringResource(R.string.create_button_label))
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text(stringResource(R.string.cancel_button_label))
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun BuildDeleteDictionaryDialog(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+        dialogTitle: String,
+        message: String
+    ){
+        AlertDialog(
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(message)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text(stringResource(R.string.dictionary_menu_delete))
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun BuildRenameDictionaryDialog(
+        onDismissRequest: () -> Unit,
+        onConfirmation: (editedTitle: String) -> Unit,
+        dialogTitle: String
+    ) {
+        val editedTitleState = rememberTextFieldState()
+        AlertDialog(
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                OutlinedTextField(
+                    state = editedTitleState,
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(8.dp),
+                    label = { Text(stringResource(R.string.rename_dictionary_hint)) }
+                )
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation(editedTitleState.text.toString())
+                    }
+                ) {
+                    Text(stringResource(R.string.create_button_label))
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text(stringResource(R.string.cancel_button_label))
+                }
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DictionaryDialogPreview() {
+    val newDictionaryTitleState = rememberTextFieldState()
+    AlertDialog(
+        title = { Text("Create Dictionary") },
+        text = {
+            OutlinedTextField(
+                state = newDictionaryTitleState,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp),
+                label = { Text(stringResource(R.string.create_dictionary_hint)) }
+            )
+        },
+        onDismissRequest = {},
+        confirmButton = {
+            TextButton(
+                onClick = {}
+            ) { Text(stringResource(R.string.create_button_label)) }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {}
+            ) { Text(stringResource(R.string.cancel_button_label)) }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun DeleteDialogPreview(){
+    AlertDialog(
+        title = {
+            Text(text = stringResource(R.string.dictionary_menu_delete))
+        },
+        text = {
+            Text(stringResource(R.string.verify_deletion))
+        },
+        onDismissRequest = {
+
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+
+                }
+            ) {
+                Text(stringResource(R.string.dictionary_menu_delete))
+            }
+        }
+    )
+}
