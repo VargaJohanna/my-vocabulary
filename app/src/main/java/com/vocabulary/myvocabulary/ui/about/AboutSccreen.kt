d6e4910be74af99bb32cc82d4cd9fa5ff2be3e6e
VargaJohanna <varga.johanna@gmail.com>
2026-02-22 17:40:56 +0100

    Add content AboutScreen.

--- app/src/main/java/com/vocabulary/myvocabulary/ui/about/AboutSccreen.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/ui/about/AboutSccreen.kt
@@ -1,6 +1,8 @@
 package com.vocabulary.myvocabulary.ui.about
 
+import android.R.attr.version
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,45 +12,46 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.vansuita.materialabout.builder.AboutBuilder
+import com.vocabulary.myvocabulary.BuildConfig
+import com.vocabulary.myvocabulary.Constants
+import com.vocabulary.myvocabulary.R
 
 @Composable
 fun AboutScreen() {
+    val context = LocalContext.current
+    val version = "${BuildConfig.VERSION_NAME} - ${BuildConfig.VERSION_CODE}"
 
-    FlowColumn(
+    AndroidView(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        ButtonCard( text = "About Screen")
-
-    }
-}
-
-
-@Composable
-fun ButtonCard(text: String) {
-    Row(modifier = Modifier
-        .padding(all = 32.dp)
-        .fillMaxWidth()
-    ) {
-        Button(onClick = {} ,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text, modifier = Modifier.padding(32.dp), fontSize = 32.sp)
+        factory = { ctx ->
+            AboutBuilder.with(ctx)
+                .setPhoto(R.drawable.profile)
+                .setCover(com.vansuita.materialabout.R.mipmap.profile_cover)
+                .addEmailLink(Constants.EMAIL)
+                .addLinkedInLink(Constants.LINKEDIN_ID)
+                .setAppIcon(R.mipmap.ic_launcher)
+                .setAppName(ctx.getString(R.string.app_name))
+                .setAppTitle(version)
+                .setActionsColumnsCount(2)
+                .setLinksAnimated(true)
+                .setLinksColumnsCount(2)
+                .setWrapScrollView(true)
+                .setShowAsCard(true)
+                .addShareAction(R.string.app_name)
+                .build()
         }
-    }
+    )
 }
 
 @Preview
 @Composable
-fun DictionaryListScreenPreview() {
-    FlowColumn(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        ButtonCard( text = "About")
-
-    }
+fun AboutPreview() {
+    AboutScreen()
 }
