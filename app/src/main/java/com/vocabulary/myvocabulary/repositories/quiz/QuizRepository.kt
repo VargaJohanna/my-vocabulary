8363e094063c00ed474721642344ebe6685fe642
VargaJohanna <varga.johanna@gmail.com>
2026-09-03 11:10:47 +0200

    Upgrade gradle version. Tidy up libs.

--- app/src/main/java/com/vocabulary/myvocabulary/repositories/quiz/QuizRepository.kt
+++ app/src/main/java/com/vocabulary/myvocabulary/repositories/quiz/QuizRepository.kt
@@ -7,6 +7,5 @@ import kotlinx.coroutines.flow.Flow
 interface QuizRepository {
     val quizList: Flow<List<Word>>
     fun updateQuizList(list: List<Word>)
-//    fun resetQuizList(dictionaryId: Long, quizType: QuizTypes): Completable
     suspend fun setQuizList(dictionaryId: Long, quizType: QuizTypes)
 }
